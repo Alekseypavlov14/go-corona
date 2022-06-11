943537eda7ec5564b26dae106e42af82b5dbc7a3
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-06-11 10:59:44 +0300

    js (burger)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './scss/index.scss'
+
+// burger 
+const burgerButton = document.getElementById('menu__icon')
+const menu = document.getElementById('menu')
+
+burgerButton.onclick = () => {
+  menu.classList.toggle('--opened')
+}
